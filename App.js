a241550d3559209e54315a410b351c74cf65c609
Matheus Alves <matheusalves2906@gmail.com>
2026-05-22 14:44:46 -0300

    estilizando o app

--- App.js
+++ App.js
@@ -1,33 +1,82 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList, Image, SafeAreaView } from "react-native";
 
 export default function App() {
   const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
     async function buscarFilmes() {
-      const response = await fetch('https://ghibliapi.vercel.app/films');
-      const dados = await response.json();
-      setFilmes(dados);
+      try {
+        const response = await fetch("https://ghibliapi.vercel.app/films");
+        const dados = await response.json();
+        setFilmes(dados);
+      } catch (error) {
+        console.log("Erro ao buscar filmes:", error);
+      }
     }
     buscarFilmes();
-  }, [])
+  }, []);
 
   return (
-    <View>
-      <Text>Lista de filmes!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>Matt Ghibli!</Text>
       <FlatList
         data={filmes}
-        renderItem={({item}) => {
-          return(
-          <View> 
-            <Text>Nome: {item.title}</Text>
-            <Text>Descrição: {item.description}</Text>
-          </View>
-          )
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.lista}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.cartao}>
+              <Text style={styles.nomeFilme}>Nome: {item.title}</Text>
+              <Image 
+                source={{ uri: item.movie_banner }}  
+                style={styles.imagem}
+                resizeMode="cover"
+              />
+              <Text style={styles.descricao}>{item.description || 'Sinopse não encontrada'}</Text>
+            </View>
+          );
         }}
       />
-    </View>
+    </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  titulo: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 15,
+    
+  },
+  lista: {
+    paddingHorizontal: 16,
+  },
+  cartao: {
+    marginBottom: 25,
+    backgroundColor: "#f9f9f9",
+    padding: 12,
+    borderRadius: 8,
+  },
+  nomeFilme: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  imagem: {
+    width: "100%",  
+    height: 200,    
+    marginVertical: 12,
+    borderRadius: 10,
+    backgroundColor: '#ddd'
+  },
+  descricao: {
+    fontSize: 14,
+    color: "#444",
+    lineHeight: 20,
+  }
+});
